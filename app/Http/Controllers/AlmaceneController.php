e05eef76ce181320baf96a276c329ec989e58846
Neymer2004 <neymerllaja@gmail.com>
2022-11-07 12:01:51 -0500

    controladores listo

---
+++ app/Http/Controllers/AlmaceneController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Almacene;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class AlmaceneController extends Controller
+{
+    //
+    function index(){
+        $almacenes = Almacene::all();
+         return view('administrador.almacenes.index', compact('almacenes'));
+    }
+    function create(){
+         return view('administrador.almacenes.create');
+    }
+    function store(Request $request){
+        $almacene = new Almacene();
+        $almacene->nombre = $request->nombre;
+        $almacene->observacion = $request->observacion;
+        $almacene->save();
+        return Redirect::route('almacenes');
+    }
+    function show(){
+
+    }
+    function edit(){
+        $almacenes = Almacene::findOrfail($id);
+        return view('administrador.almacenes.edit', compact('almacenes'));
+    }
+    function update($id, Request $request){
+        $almacene = Almacene::findOrfail($id);
+        $almacene->nombre = $request->nombre;
+        $almacene->observacion = $request->observacion;
+        $almacene->update();
+        return Redirect::route('almacenes');
+
+    }
+    function destroy(){
+        $almacene = Almacene::findOrfail($id);
+        $almacene->destroy();
+        return Redirect::route('almecenes');
+
+    }
+}
