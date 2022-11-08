00f5adaa1476f160e8cc0f302b2e86fa455f854a
0elizabeth <ferchipinzonh@gmail.com>
2022-11-08 11:50:10 -0500

    Vistas de almacen V.2

--- resources/views/administrador/almacenes/create.blade.php
+++ resources/views/administrador/almacenes/create.blade.php
@@ -2,11 +2,11 @@
 @section('title', 'Nuevo almacen')
 @section('content_header')
 <h1>Crear un almacen</h1>
-{{-- {!! Form::open(['route'->'administradores.almacenes.store', 'method'->'post']) !!} --}}
+{!! Form::open(['route'=>['administrador.almacenes.store'], 'method'=>'post']) !!}
 <label for="">Nombre</label>
 <input class="form-control" type="text" style="width:50%" name="nombre">
 <label for="">Observación</label>
 <input type="text" class="form-control" style="width:50%" name="observacion">
 <button type="submit" class="btn btn-primary" style="margin: 7px">Crear</button>
-{{-- {!! Form::close() !!} --}}
+{!! Form::close() !!}
 @endsection
