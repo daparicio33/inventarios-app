0b8c3c2c63ce1bf5bb987c40f9a94a8d769970e6
Neymer2004 <neymerllaja@gmail.com>
2022-11-10 09:57:02 -0500

    ya esta codificado el store, update, y destroy de los controladores

--- app/Http/Controllers/TmovimientoController.php
+++ app/Http/Controllers/TmovimientoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tmovimiento;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Redirect;
 
 class TmovimientoController extends Controller
@@ -22,11 +23,23 @@ class TmovimientoController extends Controller
         return view('inventarios.movimientos.tmovimientos.create',compact('sino'));
     }
     function store(Request $request){
-        $tmovimiento = new Tmovimiento();
-        $tmovimiento->nombre = $request->nombre;
-        $tmovimiento->factor = $request->factor;
-        $tmovimiento->save();
-        return Redirect::route('inventarios.movimientos.tmovimientos.index');
+        try{
+            //code...
+            DB::beginTransaction();
+            $tmovimiento = new Tmovimiento();
+            $tmovimiento->nombre = $request->nombre;
+            $tmovimiento->factor = $request->factor;
+            $tmovimiento->save();
+            DB::commit();
+        } catch (\Throwable $th){
+            //throw $th;
+            DB::rollBack();
+            return Redirect::route('inventarios.movimientos.tmovimientos.index')
+            ->with('error','ocurrio un error cuando se intento guardar los datos');
+        }
+        return Redirect::route('inventarios.movimientos.tmovimientos.index')
+        ->with('info','se guardo los datos correctamente de tmovimiento');
+
     }
     function show(){
 
@@ -36,17 +49,38 @@ class TmovimientoController extends Controller
         return view('inventarios.movimientos.tmovimientos.edit', compact('tmovimiento'));
     }
     function update($id, Request $request){
-        $tmovimiento = Tmovimiento::findOrfail($id);
-        $tmovimiento->nombre = $request->nombre;
-        $tmovimiento->factor = $request->factor;
-        $tmovimiento->update();
-        return Redirect::route('inventarios.movimientos.tmovimientos.index');
+        try {
+            //code...
+            DB::beginTransaction();
+                $tmovimiento = Tmovimiento::findOrfail($id);
+                $tmovimiento->nombre = $request->nombre;
+                $tmovimiento->factor = $request->factor;
+                $tmovimiento->update();
+            DB::commit();
+        } catch (\Throwable $th) {
+            //throw $th;
+            DB::rollBack();
+            return Redirect::route('inventarios.movimientos.tmovimientos.index')
+            ->with('error','ocurrio un error cuando se intento guardar los datos');
+
+        }
+        return Redirect::route('inventarios.movimientos.tmovimientos.index')
+        ->with('info','se actualizo los datos correctamente de tmovimiento');
 
     }
     function destroy($id){
-        $tmovimiento = Tmovimiento::findOrfail($id);
-        $tmovimiento->delete();
-        return Redirect::route('inventarios.movimientos.tmovimientos.index');
+        try {
+            //code...
+            $tmovimiento = Tmovimiento::findOrfail($id);
+            $tmovimiento->delete();
+        } catch (\Throwable $th ) {
+            //throw $th;
+            return Redirect::route('inventarios.movimientos.tmovimientos.index')
+            ->with('error','no se puede eliminar el registro');
+        }
+        return Redirect::route('inventarios.movimientos.tmovimientos.index')
+        ->with('info','se elimino el registro correctamente de tmovimiento');
+
 
     }
 }
